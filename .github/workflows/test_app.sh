22316657144f2fc46258bc90f5ea9ebfcb9e91ae
Enki-work <63624263+Enki-work@users.noreply.github.com>
2020-04-16 16:44:44 +0900

    Update test_app.sh

--- .github/workflows/test_app.sh
+++ .github/workflows/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Calculator.xcworkspace \
-            -scheme Calculator\ iOS \
+xcodebuild -project MapBoxTest.xcodeproj \
+            -scheme MapBoxTest\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
