b52bf1fff688f4c10b1921f082c017f70643a235
Enki-work <63624263+Enki-work@users.noreply.github.com>
2020-04-16 20:28:58 +0900

    リファクタリング

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,2 +1,3 @@
-swiftformat.binary_path = "Pods/SwiftFormat/CommandLineTool/swiftformat"
-swiftformat.check_format(fail_on_error: true)
+github.dismiss_out_of_range_messages
+swiftlint.config_file = '.SwiftLint.yml' # ここで.swiftlint.ymlのパスを指定
+swiftlint.lint_files inline_mode: true
