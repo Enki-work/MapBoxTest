7c0ebc9d7379fcf956f8dbd5c1934c7731baa645
Enki-work <63624263+Enki-work@users.noreply.github.com>
2020-04-16 19:22:29 +0900

    リファクタリング

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,12 +1,5 @@
-# ===== PR title =====
-warn('PR is classed as Work in Progress') if github.pr_title.include? '[WIP]'
+// Dangerfile.swift
 
-# ===== diff size =====
-warn('PRの変更量が多すぎます。PRを分割しましょう！') if git.lines_of_code > 500
+import DangerPlugin // package: https://github.com/username/DangerPlugin.git
 
-# ===== Test =====
-raise('テストが書かれていません！') if `grep -r fdescribe specs/ `.length > 1
-raise('テストが書かれていません！') if `grep -r fit specs/ `.length > 1
-# ===== Label ====
-labels = github.pr_labels
-warn('labelを選択してください！') if labels.empty?
+DangerPlugin.doYourThing()
