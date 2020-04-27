995b1ccb586ffa52cbbf7fb7f6a5377fb20b3572
Enki-work <63624263+Enki-work@users.noreply.github.com>
2020-04-27 14:57:21 +0900

    サンプルコード実装

--- MapBoxTest/Views/SideMenuViewController.swift
+++ MapBoxTest/Views/SideMenuViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 import RxSwift
 
 class SideMenuViewController: BaseViewController {
+    @IBOutlet weak var tableView: UITableView!
+    let cellTitleList: [String] = ["マイローケーション"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView.init()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+}
+
+extension SideMenuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellTitleList.count
+    }
+}
+
+extension SideMenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leftMenuListIatem", for: indexPath)
+
+        cell.textLabel?.text = cellTitleList[indexPath.row]
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
